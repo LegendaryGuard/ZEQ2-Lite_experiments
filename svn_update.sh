4ee3b4b981839cc46e2b08af24fc6e2b4de9d967
LegendaryGuard <rootuser999@gmail.com>
2022-08-25 17:37:21 +0200

    Enhance update script

--- svn_update.sh
+++ svn_update.sh
@@ -1,12 +1,5 @@
-declare svnrevinfo=$( { svn info --revision HEAD --show-item revision; } )
-declare svninfodesc=$( { svn log -r HEAD; } )
-org=$GITHUB_ACTOR:$GITHUB_TOKEN
-repo='ZEQ2-Lite'
-git add --all
-if [ $? -eq 0 ]
-then
-  echo "Success: GIT ADDED."
-else
+function stopGit()
+{
   echo "NO NEW SVN UPDATE, SKIPPING..." >&2
 
   # Get workflow IDs with status "disabled_manually"
@@ -22,7 +15,18 @@ else
       gh api repos/$org/$repo/actions/runs/$run_id -X DELETE >/dev/null
     done
   done
+}
 
+declare svnrevinfo=$( { svn info --revision HEAD --show-item revision; } )
+declare svninfodesc=$( { svn log -r HEAD; } )
+org=$GITHUB_ACTOR:$GITHUB_TOKEN
+repo='ZEQ2-Lite'
+git add --all
+if [ $? -eq 0 ]
+then
+  echo "Success: GIT ADDED."
+else
+  stopGit
   exit 0
 fi
 
@@ -33,21 +37,6 @@ then
   git push
   exit 0
 else
-  echo "NO NEW SVN UPDATE, SKIPPING..." >&2
-
-  # Get workflow IDs with status "disabled_manually"
-  workflow_ids=($(gh api repos/$org/$repo/actions/workflows | jq '.workflows[] | select(.["state"] | contains("disabled_manually")) | .id'))
-
-  for workflow_id in "${workflow_ids[@]}"
-  do
-    echo "Listing runs for the workflow ID $workflow_id"
-    run_ids=( $(gh api repos/$org/$repo/actions/workflows/$workflow_id/runs --paginate | jq '.workflow_runs[].id') )
-    for run_id in "${run_ids[@]}"
-    do
-      echo "Deleting Run ID $run_id"
-      gh api repos/$org/$repo/actions/runs/$run_id -X DELETE >/dev/null
-    done
-  done
-
+  stopGit
   exit 0
 fi
